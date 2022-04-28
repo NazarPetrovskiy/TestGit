c3f1d3d216adecc759469f9986dee3f0b3e10d1d
Nazar <nazarpetrovskyy@gmail.com>
2022-04-28 14:17:32 +0300

    Initial Project

---
+++ MyGitProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGitProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
